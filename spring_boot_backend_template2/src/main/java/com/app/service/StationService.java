ef3664334bdb1861ac3aca6ad648be275700385f
TanishqKusale <tanishqkusale10@gmail.com>
2023-12-27 11:53:02 +0530

    added

---
+++ spring_boot_backend_template2/src/main/java/com/app/service/StationService.java
@@ -0,0 +1,21 @@
+package com.app.service;
+
+import java.util.List;
+
+import com.app.DTO.StationDTO;
+import com.app.DTO.StationUpdateDTO;
+
+
+
+
+public interface StationService 
+{
+	List<StationDTO> getAllstations();
+	
+	String addStation(StationDTO dto);
+	
+	String deleteStation(Long id);
+	
+	
+
+}
